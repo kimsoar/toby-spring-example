bd06e26d7d58392979f965ab137b8564ca8cf468
kimsoar <kimsoar17@gmail.com>
2024-01-05 13:39:18 +0900

    setup toby-spring

---
+++ src/main/java/com/kimsoar/tobyspringexample/TobySpringExampleApplication.java
@@ -0,0 +1,13 @@
+package com.kimsoar.tobyspringexample;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TobySpringExampleApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TobySpringExampleApplication.class, args);
+    }
+
+}
