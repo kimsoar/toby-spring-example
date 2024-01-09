da154d78b55349b9b746d6b735da6286f968ff1e
kimsoar <kimsoar17@gmail.com>
2024-01-09 17:04:44 +0900

    1.7.5 메소드를 이용한 의존관계 주입

--- src/main/java/com/springbook/user/dao/UserDao.java
+++ src/main/java/com/springbook/user/dao/UserDao.java
@@ -7,7 +7,13 @@ import java.sql.*;
 public class UserDao {
     private ConnectionMaker connectionMaker;
 
+    public UserDao() {}
+
     public UserDao(ConnectionMaker connectionMaker) {
+        this.connectionMaker = connectionMaker;
+    }
+
+    public void setConnectionMaker(ConnectionMaker connectionMaker) {
         this.connectionMaker = connectionMaker;
     }
 
