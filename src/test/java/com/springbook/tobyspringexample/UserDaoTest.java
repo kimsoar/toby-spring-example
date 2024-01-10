ad61a6ac343891303cdcbab87d5dad97bb579aba
kimsoar <kimsoar17@gmail.com>
2024-01-10 15:03:24 +0900

    2.4.1 테스트를 위한 애플리케이션 컨텍스트 관리 - 스프링 테스트 컨텍스트 프레임워크 적용

--- src/test/java/com/springbook/tobyspringexample/UserDaoTest.java
+++ src/test/java/com/springbook/tobyspringexample/UserDaoTest.java
@@ -4,16 +4,24 @@ import com.springbook.user.dao.UserDao;
 import com.springbook.user.domain.User;
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 import java.sql.SQLException;
 
 import static org.hamcrest.Matchers.is;
 import static org.junit.Assert.assertThat;
 
+@RunWith(SpringJUnit4ClassRunner.class)
+@ContextConfiguration(locations = "/applicationContext.xml")
 public class UserDaoTest {
+    @Autowired
+    private ApplicationContext context;
     private UserDao dao;
     private User user1;
     private User user2;
@@ -21,7 +29,9 @@ public class UserDaoTest {
 
     @Before
     public void setUp() {
-        ApplicationContext context = new GenericXmlApplicationContext("applicationContext.xml");
+        System.out.println(this.context); // 하나의 애플리케이션 컨텍스트가 만들어져 모든 테스트 메소드에서 사용
+        System.out.println(this); // 테스트 메소드를 실행할 때마다 새로운 테스트 오브젝트를 생성
+
         this.dao = context.getBean("userDao", UserDao.class);
 
         this.user1 = new User("gyumee", "박성철", "springno1");
