8f7e4a6565f29d923afcde7eaa56cf9de36d4aa6
kimsoar <kimsoar17@gmail.com>
2024-01-10 12:31:55 +0900

    1.8.3 DataSource 인터페이스로 변환

--- src/main/java/com/springbook/user/dao/DaoFactory.java
+++ src/main/java/com/springbook/user/dao/DaoFactory.java
@@ -2,17 +2,28 @@ package com.springbook.user.dao;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import javax.sql.DataSource;
 
 @Configuration
 public class DaoFactory {
     @Bean
     public UserDao userDao() {
         UserDao userDao = new UserDao();
-        userDao.setConnectionMaker(connectionMaker());
+        userDao.setDataSource(dataSource());
         return userDao;
     }
 
-    private ConnectionMaker connectionMaker() {
-        return new DConnectionMaker();
+    @Bean
+    public DataSource dataSource() {
+        SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+
+        dataSource.setDriverClass(com.mysql.jdbc.Driver.class);
+        dataSource.setUrl("jdbc:mysql://localhost/springbook");
+        dataSource.setUsername("spring");
+        dataSource.setPassword("book");
+
+        return dataSource;
     }
 }
