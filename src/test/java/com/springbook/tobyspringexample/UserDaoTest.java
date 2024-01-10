cdb8056afd4e3eecb5266aa0088a6dd65632fe4e
kimsoar <kimsoar17@gmail.com>
2024-01-10 15:04:32 +0900

    2.4.1 테스트를 위한 애플리케이션 컨텍스트 관리 - @Autowired

--- src/test/java/com/springbook/tobyspringexample/UserDaoTest.java
+++ src/test/java/com/springbook/tobyspringexample/UserDaoTest.java
@@ -6,8 +6,6 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.context.ApplicationContext;
-import org.springframework.context.support.GenericXmlApplicationContext;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
@@ -21,7 +19,6 @@ import static org.junit.Assert.assertThat;
 @ContextConfiguration(locations = "/applicationContext.xml")
 public class UserDaoTest {
     @Autowired
-    private ApplicationContext context;
     private UserDao dao;
     private User user1;
     private User user2;
@@ -29,11 +26,6 @@ public class UserDaoTest {
 
     @Before
     public void setUp() {
-        System.out.println(this.context); // 하나의 애플리케이션 컨텍스트가 만들어져 모든 테스트 메소드에서 사용
-        System.out.println(this); // 테스트 메소드를 실행할 때마다 새로운 테스트 오브젝트를 생성
-
-        this.dao = context.getBean("userDao", UserDao.class);
-
         this.user1 = new User("gyumee", "박성철", "springno1");
         this.user2 = new User("leegw700", "이길원", "springno2");
         this.user3 = new User("bumjin", "박범진", "springno3");
