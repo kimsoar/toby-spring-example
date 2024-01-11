89577bffcecf7409f3928caf6cf98fefb1bca825
kimsoar <kimsoar17@gmail.com>
2024-01-12 01:30:37 +0900

    3.5.3 템플릿/콜백의 응용

---
+++ src/test/java/learningtest/template/CalcSumTest.java
@@ -0,0 +1,35 @@
+package learningtest.template;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+
+import static org.hamcrest.Matchers.is;
+import static org.junit.Assert.assertThat;
+
+public class CalcSumTest {
+    Calculator calculator;
+    String numFilepath;
+
+    @Before
+    public void setUp() {
+        this.calculator = new Calculator();
+        this.numFilepath = getClass().getResource("numbers.txt").getPath();
+    }
+
+    @Test
+    public void sumOfNumbers() throws IOException {
+        assertThat(calculator.calcSum(numFilepath), is(10));
+    }
+
+    @Test
+    public void multiplyOfNumbers() throws IOException {
+        assertThat(calculator.calcMultiply(numFilepath), is(24));
+    }
+
+    @Test
+    public void concatenateStrings() throws IOException {
+        assertThat(calculator.concatenate(numFilepath), is("1234"));
+    }
+}
